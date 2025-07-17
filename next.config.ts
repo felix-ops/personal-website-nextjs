881b8e3617493f018d6610cb81dd71d6b9d613b9
bhuvaneshwaran` m <bhuvaneshwaran.m@monkeyscience.in>
2025-07-17 23:30:19 +0530

    fix: fix lightning css missing issue

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,39 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  devIndicators: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "pixabay.com",
-        pathname: "/**", // Allow all paths under pixabay.com
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.pixabay.com",
-        pathname: "/**", 
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**", 
-      },
-      {
-        protocol: "https",
-        hostname: "via.placeholder.com",
-        pathname: "/**", 
-      },
-      {
-        protocol: "https",
-        hostname: "kastner.io",
-        pathname: "/**",
-      },
-    ],
-  },
+	/* config options here */
+	devIndicators: false,
+	// Disable native minifier to avoid lightningcss or SWC native issues
+	swcMinify: false,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "pixabay.com",
+				pathname: "/**", // Allow all paths under pixabay.com
+			},
+			{
+				protocol: "https",
+				hostname: "cdn.pixabay.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "images.unsplash.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "via.placeholder.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "kastner.io",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
